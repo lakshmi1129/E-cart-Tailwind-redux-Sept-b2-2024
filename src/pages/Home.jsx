e10e962dbf1425d7f7db7ffea1db99f311a891ec
lakshmi2911 <lakshmipriya1129@gmail.com>
2025-01-30 11:45:40 +0530

    complete

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,76 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Header from '../components/Header'
+import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchProducts } from '../redux/slices/productSlice'
+
 
 const Home = () => {
+  const dispatch = useDispatch()
+  const {allProducts,loading,errorMsg}= useSelector(state=>state.productReducer)
+  // console.log(allProducts,loading,errorMsg);
+  // pagination
+  const [currentPage,setCurrentPage]= useState(1)
+  const productPerPage = 8
+  const totalpages = Math.ceil(allProducts?.length/productPerPage)
+  const currentPageProductLastIndex = currentPage* productPerPage
+  const currentPageProductFirstIndex = currentPageProductLastIndex-productPerPage
+  const visibleAllProducts = allProducts?.slice(currentPageProductFirstIndex,currentPageProductLastIndex)
+  
+  
+  useEffect(()=>{
+      dispatch(fetchProducts())
+  },[])
+
+  const navigateToNextPage =()=>{
+    if(currentPage!=totalpages){
+      setCurrentPage(currentPage+1)
+    }
+  }
+  const navigateToPrevPage =()=>{
+    if(currentPage!=1){
+      setCurrentPage(currentPage-1)
+    }
+  }
   return (
-    <div>Home</div>
+    <>
+    <Header insideHome ={true}/>
+    <div style={{paddingTop:'100px'}} className='container px-4 mx-auto'>
+      {
+        loading?
+        <div className='flex justify-center items-center my-5 text-3xl'>
+          <img width={'200px'} height={'200px'} src="https://wpamelia.com/wp-content/uploads/2018/11/ezgif-2-6d0b072c3d3f.gif" alt="" />
+            Loading.....
+        </div>
+        :
+        <>
+            <div className='grid grid-cols-4 gap-4'>
+              {
+                allProducts.length>0 ?
+                visibleAllProducts?.map(product=>(
+                  <div className='rounded border p-2 shadow'>
+                    <img width={'100%'} height={'200px'} src={product?.thumbnail} alt="" />
+                    <div className='text-center'>
+                      <h3 className='text-xl font-bold'>{product?.title}</h3>
+                      <Link to={`/${product?.id}/view`} className='bg-violet-600 rounded p-1 mt-3 text-white inline-block'> View more...</Link>
+                    </div>
+                  </div>
+                ))
+                  :
+                  <div className='flex justify-center items-center font-bold text-red-600 my-5 text-lg'>
+                    Products not found!!!
+                  </div>
+              }
+            </div>
+            <div className='text-2xl text-center font-bold mt-20'>
+              <span onClick={navigateToPrevPage} className='cursor-pointer'> <i className='fa-solid fa-backward me-5'></i></span>
+              <span> {currentPage} of {totalpages}</span>
+              <span onClick={navigateToNextPage} className='cursor-pointer'> <i className='fa-solid fa-forward ms-5'></i></span>
+            </div>
+        </>
+      }
+    </div>
+    </>
   )
 }
 
--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -1,8 +1,60 @@
 import React from 'react'
+import Header from '../components/Header'
+import { useDispatch, useSelector } from 'react-redux'
+import { removeItem } from '../redux/slices/wishlistSlice'
+import { addToCart } from '../redux/slices/cartSlice'
+
+
 
 const Wishlist = () => {
+  const userCart = useSelector(state=>state.cartReducer)
+  const dispatch = useDispatch()
+  const userWishlist = useSelector(state=>state.wishlistReducer)
+
+  const handleCart =(product)=>{
+     dispatch(addToCart(product))
+        const existingProduct = userCart?.find(item=>item.id==product.id)
+        if(existingProduct){
+          alert("Product Quantity incremented")
+        }else{
+          alert('Product add to  your cart')
+        }
+  }
+
+
   return (
-    <div>Wishlist</div>
+    <>
+      <Header/>
+      <div style={{paddingTop:'100px'}} className='px-5'>
+       {
+        userWishlist?.length>0 ?
+            <>
+                <h1 className='text-4xl font-bold text-red-600'>My Wishlist</h1>
+                <div className='grid grid-cols-4 gap-4'>
+                  {
+                    userWishlist?.map(product=>(
+                      <div className='rounded border p-2 shadow'>
+                    <img width={'100%'} height={'200px'} src={product?.thumbnail} alt="" />
+                    <div className='text-center'>
+                        <h3 className='text-xl font-bold'>{product?.title}</h3>
+                        <div className='flex justify-evenly mt-3'>
+                          <button onClick={()=>dispatch(removeItem(product?.id))} className='text-xl'><i className='fa-solid fa-heart-circle-xmark text-red-600'></i></button>
+                          <button onClick={()=>handleCart(product)} className='text-xl'><i className='fa-solid fa-cart-plus text-green-600'></i></button>
+                        </div>
+                      </div>
+                    </div>
+                    ))
+                  }
+                </div>
+            </>
+            :
+            <div  className='flex justify-center items-center h-screen'>
+              <img className='w-100 h-1/2' src="https://assets-v2.lottiefiles.com/a/6102a4f8-1176-11ee-bcc5-236dd7d5f88b/aK8IKRE5a3.gif" alt="" />
+              <h1 className='text-3xl text-red-600'>Your wishlist is empty!!!</h1>
+            </div>
+       }
+      </div>
+    </>
   )
 }
 
